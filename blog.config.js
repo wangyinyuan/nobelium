61130bba8c462edf524a75e504976f4308d54e69
王鑫 <110143758+wangyinyuan@users.noreply.github.com>
2023-08-09 11:27:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'Bug日志',
+  author: 'LiuQi',
+  email: '2972437973@qq.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '本站主要收录作者在编程时遇到的一些问题，包括一些编程错误和环境配置的坑，也会有一些IT技术分享。 The main content of this site includes some problems encountered by the author during programming, including some programming errors and pitfalls in environment configuration, as well as some IT technology sharing.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
